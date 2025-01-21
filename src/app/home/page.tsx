e938bd66a3fea08ef637f68069c57e1d883616d9
Vamsi Batchu <vamsibatchuk@gmail.com>
2025-01-21 18:15:40 -0500

    Remove unused Benne font import in home page component and update TypingAnimation usage.

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,16 +1,17 @@
 "use client";
 
 import React from "react";
-import localFont from "next/font/local";
+//import localFont from "next/font/local";
 import { TypingAnimation } from "@/components/ui/typing-animation";
 import { RetroGrid } from "@/components/ui/retro-grid";
 export type IconProps = React.HTMLAttributes<SVGElement>;
 
-const benne = localFont({
+/*const benne = localFont({
   src: '../../../src/fonts/benne.woff2',
   display: "swap",
   variable: "--font-benne",
 });
+*/
 
 export default function Page() {
   return (
@@ -19,7 +20,7 @@ export default function Page() {
       <section className="min-h-screen flex items-center justify-center relative">
         <RetroGrid className="z-2" />
         <div className="container mx-auto px-4 relative z-10">
-          <TypingAnimation className={benne.className} width="50%">
+          <TypingAnimation>
             Hello, I am a hands-on product design leader with ten+ years of
             experience in designing and leading teams developing highly
             impactful products at scale.Currently, leading the enterprise and AI
@@ -59,3 +60,5 @@ export default function Page() {
     </main>
   );
 }
+
+//{benne.className}
